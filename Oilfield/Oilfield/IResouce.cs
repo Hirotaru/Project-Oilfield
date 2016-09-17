9a366f85bd59dd41a322aa1191ecf9fd472ea625
Hirotaru <7460624713681YZ$@(((8!>
2016-09-17 13:47:34 +0300

    Basic classes fix

---
+++ Oilfield/Oilfield/IResouce.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Oilfield
+{
+    public interface IResouce
+    {
+        Point Position { get; set; }
+
+        double Depth { get; set; }
+
+        double Amount { get; set; }
+
+
+    }
+}
--- Oilfield/Oilfield/Waterfield.cs
+++ Oilfield/Oilfield/Waterfield.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing;
 
 namespace Oilfield
 {
-    class Waterfield
+    class Waterfield : IResouce
     {
+        private Point position;
+
+        public Point Position
+        {
+            get { return position; }
+            set { }
+        }
+
+        private double amount;
+
+        public double Amount
+        {
+            get { return amount; }
+            set { }
+        }
+
+        private double depth;
+
+        public double Depth
+        {
+            get { return depth; }
+            set { }
+        }
+
     }
 }
