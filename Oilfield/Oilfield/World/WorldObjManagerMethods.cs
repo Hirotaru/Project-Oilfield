af92f60f57dbdf030f8e2eb3a8d9f5d5c6d98a5f
Hirotaru <7460624713681YZ$@(((8!>
2016-12-15 19:18:11 +0300

    World Update

---
+++ Oilfield/Oilfield/World/WorldObjManagerMethods.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Oilfield
+{
+
+    public partial class World
+    {
+
+        private List<IResource> convertObjectToResource(List<IObject> obj)
+        {
+            List<IResource> res = new List<IResource>();
+
+            foreach (var item in obj)
+            {
+                res.Add(item as IResource);
+            }
+
+            return res;
+        }
+        public List<IResource> GetBetterAnalysis(ResourceType type)
+        {
+            var a = objManager.GetBetterAnalysis(type);
+
+            return convertObjectToResource(a);
+        }
+
+        public List<IResource> GetBetterChemicalAnalysis(ResourceType type)
+        {
+            var a = objManager.GetBetterChemicalAnalysis(type);
+
+            return convertObjectToResource(a);
+        }
+
+        public List<IResource> GetBetterOverallAnalysis(ResourceType type)
+        {
+            var a = objManager.GetBetterOverallAnalysis(type);
+
+            return convertObjectToResource(a);
+        }
+    }
+}
